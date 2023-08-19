3d16e259977871ae43259f08fd2acb21b50a5b42
YarinKeren <jarin4378@gmail.com>
2023-08-19 17:24:35 +0200

    Finished assignments raw

--- SITE/js/app.js
+++ SITE/js/app.js
@@ -1,3 +1,14 @@
+const gElMain = document.querySelector('.main-content')
+
 function toggleMenu() {
   document.body.classList.toggle('menu-open')
+}
+
+function showModal() {
+  console.log(gElMain)
+  document.body.classList.add('modal-open')
+}
+
+function closeModal() {
+  document.body.classList.remove('modal-open')
 }
